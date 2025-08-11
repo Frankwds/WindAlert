3a77ef5da59711738c53baa63a891243d18ee039
Frank William Daniels <frank.william.daniels@gmail.com>
2025-08-11 11:26:47 +0200

    Define base types for yr data

--- src/app/api/cron/types/yr.ts
+++ src/app/api/cron/types/yr.ts
@@ -1,43 +1,34 @@
-export interface WeatherDataPointYr {
+interface BaseWeatherDataPoint {
   time: string;
   air_pressure_at_sea_level: number;
   air_temperature: number;
+  air_temperature_percentile_10: number;
+  air_temperature_percentile_90: number;
   cloud_area_fraction: number;
   cloud_area_fraction_high: number;
   cloud_area_fraction_low: number;
   cloud_area_fraction_medium: number;
   dew_point_temperature: number;
-  fog_area_fraction: number;
   relative_humidity: number;
-  ultraviolet_index_clear_sky: number;
   wind_from_direction: number;
   wind_speed: number;
-  wind_speed_of_gust: number;
-  symbol_code: string;
   precipitation_amount: number;
   precipitation_amount_max: number;
   precipitation_amount_min: number;
   probability_of_precipitation: number;
+  symbol_code: string;
+}
+
+export interface WeatherDataPointYr extends BaseWeatherDataPoint {
+  fog_area_fraction: number;
+  ultraviolet_index_clear_sky: number;
+  wind_speed_of_gust: number;
   probability_of_thunder: number;
 }
 
-export interface WeatherDataPointYr6h { // Try with 54 in slices in the array, if not write a helper func to decide when to switch.
-  time: string;
-  air_pressure_at_sea_level: number;
-  air_temperature: number;
-  cloud_area_fraction: number;
-  cloud_area_fraction_high: number;
-  cloud_area_fraction_low: number;
-  cloud_area_fraction_medium: number;
-  dew_point_temperature: number;
-  relative_humidity: number;
-  wind_from_direction: number;
-  wind_speed: number;
-  wind_speed_of_gust: number;
-  symbol_code: string;
-  precipitation_amount: number;
-  precipitation_amount_max: number;
-  precipitation_amount_min: number;
-  probability_of_precipitation: number;
-  probability_of_thunder: number;
+export interface WeatherDataPointYr6h extends BaseWeatherDataPoint {
+  wind_speed_percentile_10: number;
+  wind_speed_percentile_90: number;
+  air_temperature_max: number;
+  air_temperature_min: number;
 }
