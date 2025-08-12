487b448c4676f77cea8ca56b0ccf6dd1ed861cde
Frank William Daniels <frank.william.daniels@gmail.com>
2025-08-12 12:30:23 +0200

    make tests more readable

--- src/app/api/cron/lib/utils/windDirection.test.ts
+++ src/app/api/cron/lib/utils/windDirection.test.ts
@@ -1,4 +1,5 @@
 import { isWindDirectionGood } from '@/app/api/cron/lib/utils/windDirection';
+import { windDirectionMapping as dirMap } from '@/app/lib/utils/windDirection';
 
 // Test all directions
 const dir = { 'N': 0, 'NE': 45, 'E': 90, 'SE': 135, 'S': 180, 'SW': 225, 'W': 270, 'NW': 315 };
@@ -10,32 +11,32 @@ for (const [direction, angle] of Object.entries(dir)) {
 
 describe('isWindDirectionGood', () => {
   it('should return true when allowedDirections is empty', () => {
-    expect(isWindDirectionGood(180, [])).toBe(true);
+    expect(isWindDirectionGood(dir.S, [])).toBe(true);
   });
 
   it('should return true for a wind direction that is within the allowed range for a single allowed direction', () => {
-    expect(isWindDirectionGood(90, ['E'])).toBe(true);
+    expect(isWindDirectionGood(dir.E, ['E'])).toBe(true);
   });
 
   it('should return false for a wind direction that is outside the allowed range for a single allowed direction', () => {
-    expect(isWindDirectionGood(180, ['E'])).toBe(false);
+    expect(isWindDirectionGood(dir.W, ['E'])).toBe(false);
   });
 
   it('should handle the North direction correctly when it crosses 360 degrees', () => {
-    expect(isWindDirectionGood(350, ['N'])).toBe(true);
-    expect(isWindDirectionGood(10, ['N'])).toBe(true);
-    expect(isWindDirectionGood(45, ['N'])).toBe(false);
+    expect(isWindDirectionGood(dir.N - 10, ['N'])).toBe(true);
+    expect(isWindDirectionGood(dir.N, ['N'])).toBe(true);
+    expect(isWindDirectionGood(dir.NE, ['N'])).toBe(false);
   });
 
   it('should return true for a wind direction that is within any of the allowed ranges for multiple allowed directions', () => {
-    expect(isWindDirectionGood(45, ['N', 'NE', 'E'])).toBe(true);
-    expect(isWindDirectionGood(90, ['N', 'NE', 'E'])).toBe(true);
+    expect(isWindDirectionGood(dir.NE, ['N', 'NE', 'E'])).toBe(true);
+    expect(isWindDirectionGood(dir.E, ['N', 'NE', 'E'])).toBe(true);
     expect(isWindDirectionGood(340, ['N', 'NE', 'E'])).toBe(true);
   });
 
   it('should work fine, also when North is added after other directions', () => {
-    expect(isWindDirectionGood(45, ['NE', 'E', 'N'])).toBe(true);
-    expect(isWindDirectionGood(90, ['NE', 'E', 'N'])).toBe(true);
+    expect(isWindDirectionGood(dir.NE, ['NE', 'E', 'N'])).toBe(true);
+    expect(isWindDirectionGood(dir.E, ['NE', 'E', 'N'])).toBe(true);
     expect(isWindDirectionGood(340, ['NE', 'N', 'E'])).toBe(true);
     expect(isWindDirectionGood(10, ['NE', 'N', 'E'])).toBe(true);
   });
@@ -57,16 +58,16 @@ describe('isWindDirectionGood', () => {
 
   it('should handle boundary conditions correctly', () => {
     // Test lower and upper bounds for each direction
-    expect(isWindDirectionGood(22.5, ['NE'])).toBe(true); // Lower bound of NE
-    expect(isWindDirectionGood(67.5, ['NE'])).toBe(true); // Upper bound of NE
-    expect(isWindDirectionGood(22.4, ['NE'])).toBe(false);
-    expect(isWindDirectionGood(67.6, ['NE'])).toBe(false);
+    expect(isWindDirectionGood(dirMap.NE.min, ['NE'])).toBe(true); // Lower bound of NE
+    expect(isWindDirectionGood(dirMap.NE.max, ['NE'])).toBe(true); // Upper bound of NE
+    expect(isWindDirectionGood(dirMap.NE.min - 0.1, ['NE'])).toBe(false);
+    expect(isWindDirectionGood(dirMap.NE.max + 0.1, ['NE'])).toBe(false);
 
     // Test North boundary
-    expect(isWindDirectionGood(337.5, ['N'])).toBe(true); // Lower bound of N
-    expect(isWindDirectionGood(22.5, ['N'])).toBe(true); // Upper bound of N
-    expect(isWindDirectionGood(337.4, ['N'])).toBe(false);
-    expect(isWindDirectionGood(22.6, ['N'])).toBe(false);
+    expect(isWindDirectionGood(dirMap.N.min, ['N'])).toBe(true); // Lower bound of N
+    expect(isWindDirectionGood(dirMap.N.max, ['N'])).toBe(true); // Upper bound of N
+    expect(isWindDirectionGood(dirMap.N.min - 0.1, ['N'])).toBe(false);
+    expect(isWindDirectionGood(dirMap.N.max + 0.1, ['N'])).toBe(false);
   });
 
 
