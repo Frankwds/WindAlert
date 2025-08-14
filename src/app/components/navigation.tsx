45744417e0ba1062c2b441ee2d82af38dfff82c5
Frank William Daniels <frank.william.daniels@gmail.com>
2025-08-14 11:42:49 +0200

    Add windlord.png

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 const links = [
@@ -12,16 +13,29 @@ export default function Navigation() {
   const pathname = usePathname();
 
   return (
-    <nav className="p-4 bg-gray-800 text-white">
-      <ul className="flex space-x-4">
-        {links.map(({ href, label }) => (
-          <li key={href}>
-            <Link href={href} className={pathname === href ? "underline" : ""}>
-              {label}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <nav className="px-4 bg-gray-800 text-white">
+      <div className="flex items-center py-2">
+        <div className="mr-4">
+          <Image
+            src="/windlord-dark.png"
+            alt="WindLord Logo"
+            width={64}
+            height={64}
+          />
+        </div>
+        <ul className="flex space-x-4">
+          {links.map(({ href, label }) => (
+            <li key={href}>
+              <Link
+                href={href}
+                className={pathname === href ? "underline" : ""}
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 }
