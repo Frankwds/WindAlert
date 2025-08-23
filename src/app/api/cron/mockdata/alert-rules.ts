4a71a2203e6de4a6b5dcec51f69beafea85a14a1
Frank Daniels <frank.william.daniels@gmail.com>
2025-08-23 10:33:15 +0200

    updatre mockdata

--- src/app/api/cron/mockdata/alert-rules.ts
+++ src/app/api/cron/mockdata/alert-rules.ts
@@ -1,62 +1,56 @@
 import { AlertRule } from '@/lib/common/types/alertRule';
 
 const DEFAULT_VALUES = {
-    MIN_WIND_SPEED: 0,
-    MAX_WIND_SPEED: 8,
-    MAX_GUST: 10.0,
-    MAX_GUST_DIFFERENCE: 4.0,
-    MAX_PRECIPITATION: 0,
-    MAX_CAPE: 10000,
-    MIN_LIFTED_INDEX: -6,
-    MAX_LIFTED_INDEX: 8,
-    MIN_CONVECTIVE_INHIBITION: -5000,
-    MAX_CLOUD_COVER: 100,
-    MAX_WIND_SPEED_925hPa: 15, // 925hPa is approximately 800m altitude
-    MAX_WIND_SPEED_850hPa: 20, // 850hPa is approximately 1500m altitude
-    MAX_WIND_SPEED_700hPa: 25, // 700hPa is approximately 3000m altitude
+  MIN_WIND_SPEED: 0,
+  MAX_WIND_SPEED: 8,
+  MAX_GUST: 10.0,
+  MAX_GUST_DIFFERENCE: 4.0,
+  MAX_PRECIPITATION: 0,
+  MAX_CAPE: 10000,
+  MIN_LIFTED_INDEX: -6,
+  MAX_LIFTED_INDEX: 8,
+  MIN_CONVECTIVE_INHIBITION: -5000,
+  MAX_CLOUD_COVER: 100,
+  MAX_WIND_SPEED_925hPa: 15, // 925hPa is approximately 800m altitude
+  MAX_WIND_SPEED_850hPa: 20, // 850hPa is approximately 1500m altitude
+  MAX_WIND_SPEED_700hPa: 25, // 700hPa is approximately 3000m altitude
 };
 
 export const ALERT_RULES: AlertRule[] = [
-    {
-        id: 1,
-        locationId: 1,
-        ...DEFAULT_VALUES,
-        alert_name: "Alert for Keiservarden, Bodø",
-    },
-    {
-        id: 2,
-        locationId: 2,
-        ...DEFAULT_VALUES,
-        alert_name: "Alert for Hoven, Gimsøya",
-    },
-    {
-        id: 3,
-        locationId: 3,
-        ...DEFAULT_VALUES,
-        alert_name: "Alert for Liaset, Voss",
-    },
-    {
-        id: 4,
-        locationId: 4,
-        ...DEFAULT_VALUES,
-        alert_name: "Alert for Hangur, sør-start, Voss",
-    },
-    {
-        id: 5,
-        locationId: 5,
-        ...DEFAULT_VALUES,
-        alert_name: "Alert for Salknappen (Riksanlegget), Vågå",
-    },
-    {
-        id: 6,
-        locationId: 6,
-        ...DEFAULT_VALUES,
-        alert_name: "Alert for Grøtterud, Hvittingfoss",
-    },
-    {
-        id: 7,
-        locationId: 7,
-        ...DEFAULT_VALUES,
-        alert_name: "Alert for Sundvollen, Viken",
-    },
+  {
+    id: 1,
+    locationId: 1,
+    ...DEFAULT_VALUES,
+    alert_name: '',
+  },
+  {
+    id: 2,
+    locationId: 2,
+    ...DEFAULT_VALUES,
+    alert_name: '',
+  },
+  {
+    id: 3,
+    locationId: 3,
+    ...DEFAULT_VALUES,
+    alert_name: '',
+  },
+  {
+    id: 4,
+    locationId: 4,
+    ...DEFAULT_VALUES,
+    alert_name: '',
+  },
+  {
+    id: 5,
+    locationId: 5,
+    ...DEFAULT_VALUES,
+    alert_name: '',
+  },
+  {
+    id: 6,
+    locationId: 6,
+    ...DEFAULT_VALUES,
+    alert_name: '',
+  },
 ];
